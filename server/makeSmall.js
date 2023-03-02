fac13f51978ac98e7c1472c840952640d129e8ae
Sean Stickland <sticklandsm@gmail.com>
2023-03-02 20:55:49 +1300

    did a bunch of weird editing with four letter words

--- server/makeSmall.js
+++ server/makeSmall.js
@@ -1,5 +1,7 @@
 const fs = require('node:fs/promises')
 
+badLetters = ['d', 'k', 'm', 'n', 'q', 'r', 't', 'v', 'w', 'x']
+
 makeItSo()
 
 async function readFile(file) {
@@ -10,7 +12,7 @@ async function readFile(file) {
 async function writeFile(newObj) {
   console.log(Object.keys(newObj).length)
   fs.writeFile(
-    'public/data/SimpleFourDef.json',
+    'public/data/FourLettersNoMatchSticks.json',
     JSON.stringify(newObj),
     (err) => {
       if (err) {
@@ -23,22 +25,27 @@ async function writeFile(newObj) {
 
 async function makeItSo() {
   try {
-    let allWords = await readFile('public/data/fourLetterWords.json')
+    // let allWords = await readFile('public/data/fourLetterWords.json')
     let simpleWords = await readFile('public/data/SimpleFourDef.json')
 
     const keys = Object.keys(simpleWords)
-    const values = Object.values(simpleWords)
-    let fourLetterWords = {}
+    // const values = Object.values(simpleWords)
+    let goodWords = {}
     let i = 0
-    console.log(keys[1000], values[1000], 'simple Words')
+    // console.log(keys[1000], values[1000], 'simple Words')
 
-    // keys.forEach((key) => {
-    //   if (simpleWords.includes(key.toUpperCase())) {
-    //     fourLetterWords[key] = values[i]
-    //   }
-    //   i++
-    // })
-    // writeFile(await fourLetterWords)
+    // const hasTheLetters = (word, letters) => {
+    //   return letters.some((letter) => word.includes(letter))
+    // }
+
+    let newArr = keys.filter((key) => {
+      return !badLetters.some((badLetter) => key.includes(badLetter))
+    })
+    newArr.forEach((item) => {
+      goodWords[item] = simpleWords[item]
+    })
+    console.log(Object.keys(goodWords).length)
+    writeFile(goodWords)
     console.log('words saved', ' data is saved. in theory')
   } catch (err) {
     console.log(err)
